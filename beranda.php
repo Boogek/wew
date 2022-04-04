d62fc23f30ca343485717d18dce1ace5677014ab
Boogek <102939851+Boogek@users.noreply.github.com>
2022-04-04 16:04:23 +0800

    Add files via upload

---
+++ beranda.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+
+
+    <title>Cuma tugas</title>
+  </head>
+  <body class="mt-8">
+
+    <!-- navbar -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-danger">
+   
+    <div class="container">
+    <a class="navbar-brand" href="#">KAMISAMA</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNavAltMarkup ">
+      <div class="navbar-nav ">
+        <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+    </div>
+  </nav>
+    <!-- akhir navbar -->
+
+    <!-- jumbotron -->
+    <div class="jumbotron jumbotron-fluid shadow ">
+      <div class="container text-center">
+        <img src="img/logo.png" width="25%" class="rounded-circle img-thumbnail">
+        <h1 class="display-4">7 deadly sins</h1>
+        <p class="lead">Gregorius menghilangkan "kemalasan" dan menambahkan "iri hati." 
+Ia pun menjadikan "kesombongan" pada tempatnya sendiri dalam daftar. Terpisah, "kesombongan" digambarkannya sebagai penguasa dari tujuh kejahatan sisanya. Lantas, kita pun mengenal sisa ini  sebagai tujuh dosa mematikan.
+Revisi atas gagasan itu terjadi pada abad ke-13, ketika itu teolog Thomas Aquinas meninjau kembali daftar di Summa Theologica ("Ringkasan Teologi"). Dalam daftarnya, Aquinas memasukkan kembali "kemalasan" dan menghilangkan "kesedihan". 
+ADVERTISEMENT
+Seperti Gregorius, Aquinas pun menggambarkan "kesombongan" secara terpisah, sebagai penguasa tertinggi dari tujuh dosa</p>
+      </div>
+    </div>
+    <!-- akhir jumbotron -->
+
+<!-- lust -->
+      <div class="row mb-5">
+        <div class="col-md">
+        <div class="card" >
+        <img src="img/iblis.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h1 class="text-center"> Lust </h1>
+            <p class="card-text text-justify">Lust (Luxuria) adalah hasrat yang hebat dan tidak terkontrol. Sering dikenal sebagai keinginan seksual yang tidak dapat dikendalikan. Tetapi kata lust sebenarnya adalah istilah umum untuk “Nafsu“. Untuk itu, Lust dapat berarti nafsu yang tidak terkendali pada uang, makanan, ketenaran, dan kekuatan. </p>
+          </div>
+        </div>
+      </div>
+<!-- akhir lust -->
+
+<!-- gluthony -->
+        <div class="col-md">
+        <div class="card" >
+          <img src="img/iblis1.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h1 class="text-center"> Gluttony </h1>
+            <p class="card-text text-center">Berasal dari bahasa Latin ‘Gluttire‘ yang berarti menelan, Gluttony (gula) adalah makan yang berlebihan. Istilah lainnya rakus.
+Ada 6 ‘makan’ yang termasuk pada gluttony, yaitu :
+Praepropere ->makan terlalu cepat.
+Laute ->makan terlalu mahal.
+Nimis ->makan terlalu banyak.
+Ardenter ->makan terlalu bernafsu.
+Studiose ->makan terlalu manis.
+Forente ->makan terlalu berisik.</p>
+          </div>
+        </div>
+      </div>
+<!-- ahkir -->
+
+<!-- awal -->
+        <div class="col-md">
+        <div class="card" >
+          <img src="img/iblis2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h1 class="text-center"> Greed </h1>
+            <p class="card-text text-center">Greed (Avaritia) dapat berarti ketamakan dan keserakahan. Semacam keinginan berlebihan untuk mengejar materi. Ketamakan atau keserakahan ini dapat menyebabkan kekerasan, penipuan, dan manipulasi kekuasaan karena orang yang sudah terbudak oleh sifat serakahnya akan menghalalkan segala cara untuk mendapatkan apa yang diinginkannya. </p>
+          </div>
+        </div>
+        </div>
+      </div>
+<!-- akhir -->
+
+<!-- awal -->
+      <div class="row mb-5">
+        <div class="col-md">
+        <div class="card" >
+          <img src="img/iblis3.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h1 class="text-center"> Sloth </h1>
+            <p class="card-text text-justify">Sloth (acedia) dapat didefinisikan sebagai kemalasan fisikal dan spiritual. Sloth juga dapat berarti kegagalan dalam melakukan sesuatu yang harus dilakukan —tanpa berusaha tentunya.</p>
+          </div>
+        </div>
+      </div>
+
+        <div class="col-md">
+        <div class="card" >
+          <img src="img/iblis4.png" class="card-img-top" alt="...">
+          <div class="card-body">
+          <h1 class="text-center"> Wrath </h1>
+            <p class="card-text text-justify">Wrath (ira) adalah perasaan kebenciaan dan amarah yang tidak terkontrol. Wrath dipresentasikan sebagai perusakan diri, kekerasan, dan kebencian yang dapat memancing permusuhan selama berabad-abad.
+Perasaan marah dapat berwujud macam-macam seperti ketidaksabaran, dendam, dan perusakan diri sendiri, seperti: Penyalahgunaan narkoba dan bunuh diri.</p>
+          </div>
+        </div>
+        </div>
+
+        <div class="col-md">
+        <div class="card" >
+          <img src="img/iblis5.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h1 class="text-center"> Envy </h1>
+            <p class="card-text text-justify">Envy (invidia) adalah rasa iri hati. Sama juga dengan kecemburuan terhadap pembawaan, status, kelebihan, dan penghargaan yang dimiliki orang lain. Envy juga dapat dijelaskan sebagai rasa tidak puas akan yang dimiliki diri sendiri sehingga ingin memiliki apa yang dimiliki oleh orang lain.</p>
+        </div>
+        </div>
+        </div>
+     </div>
+<!-- akhir -->
+    </section>
+  </body>
+</html>
+
